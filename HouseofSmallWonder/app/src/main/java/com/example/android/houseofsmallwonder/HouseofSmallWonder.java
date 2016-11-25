83e9c98268e6628f9656e0d973045ea14b53e4fa
Faith Xu <piscesskybelle@gmail.com>
2016-11-25 19:34:35 +0800

    Add files via upload

---
+++ HouseofSmallWonder/app/src/main/java/com/example/android/houseofsmallwonder/HouseofSmallWonder.java
@@ -0,0 +1,13 @@
+package com.example.android.houseofsmallwonder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HouseofSmallWonder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_houseof_small_wonder);
+    }
+}
